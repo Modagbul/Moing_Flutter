e4f238cb2f8c6886795f1ebb605bfec437a143db
hyunseokSon <fairy0724@soongsil.ac.kr>
2023-10-22 13:29:15 +0900

    fcm token 발급

--- lib/model/sign_up_arguments.dart
+++
@@ -1,6 +0,0 @@
-class SignUpData {
-  final String nickName;
-  final String gender;
-  final String birthDate;
-  SignUpData({required this.nickName, required this.gender, required this.birthDate});
-}
