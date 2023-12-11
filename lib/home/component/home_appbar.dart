8921551a3715a4267a3418777eb698a6d4f6eb17
hyunseokSon <fairy0724@soongsil.ac.kr>
2023-12-11 19:47:40 +0900

    HomeAppBar Row 수정

--- lib/home/component/home_appbar.dart
+++ lib/home/component/home_appbar.dart
@@ -8,8 +8,8 @@ class HomeAppBar extends StatelessWidget {
   const HomeAppBar({
     required this.notificationCount,
     required this.onTap,
-    super.key});
-
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -17,50 +17,44 @@ class HomeAppBar extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         SvgPicture.asset(
-            'asset/icons/home_moing_logo.svg',
+          'asset/icons/home_moing_logo.svg',
           width: 80,
           height: 32,
         ),
         const Spacer(),
-        Stack(
-          alignment: Alignment.center,
-          children: [
-            Container(
-              width: 21,
-              height: 25,
-              decoration: const BoxDecoration(
-                color: Color(0xffFF6464),
-                shape: BoxShape.circle,
+        GestureDetector(
+          onTap: onTap,
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Container(
+                width: 21,
+                height: 25,
+                decoration: const BoxDecoration(
+                  color: Color(0xffFF6464),
+                  shape: BoxShape.circle,
+                ),
               ),
-            ),
-            Text(
-              notificationCount,
-              style: const TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.w700,
+              Text(
+                notificationCount,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w700,
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
-        const SizedBox(width: 4.0,),
+        const SizedBox(width: 4.0),
         GestureDetector(
           onTap: onTap,
           child: SvgPicture.asset(
-            'asset/image/icon_notification.svg',
-            width: 24.0,
-            height: 24.0,
+            'asset/icons/home_notification.svg',
+            width: 24,
+            height: 24,
           ),
-          const Spacer(),
-          GestureDetector(
-            onTap: onTap,
-            child: SvgPicture.asset(
-              'asset/icons/home_notification.svg',
-              width: 24,
-              height: 24,
-            ),
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
