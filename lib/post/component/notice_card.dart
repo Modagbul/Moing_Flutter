2d78bc8a2307706dff1639469954269963c6e5d6
yeopseung <kwak991021@gmail.com>
2024-01-02 13:32:52 +0900

    Feat: 게시글/공지, 댓글 신고 Alert 바텀 시트 추가

--- lib/post/component/notice_card.dart
+++ lib/post/component/notice_card.dart
@@ -22,6 +22,7 @@ class NoticeCard extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.only(left: 16.0, bottom: 16.0, right: 14.0),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             _renderNoticeCardHeader(nickName: noticeData.writerNickName),
--- lib/post/post_detail_page.dart
+++ lib/post/post_detail_page.dart
@@ -212,7 +212,7 @@ class PostDetailPage extends StatelessWidget {
       leading: IconButton(
         icon: const Icon(Icons.arrow_back),
         onPressed: () {
-          Navigator.of(context).pop();
+          Navigator.of(context).pop(true);
         },
       ),
       actions: [
@@ -252,7 +252,8 @@ class PostDetailPage extends StatelessWidget {
               children: [
                 GestureDetector(
                   onTap: () {
-                    context.read<PostDetailState>().reportPost(
+                    context.read<PostDetailState>().showReportPostModal(
+                          context: context,
                           reportType: "BOARD",
                           targetId: context.read<PostDetailState>().boardId,
                         );
